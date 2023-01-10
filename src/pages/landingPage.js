10ae2dd16b19b6a70d0df58e05d002dfbe7f0903
unknown <raihanfadhilmaftuh03@gmail.com>
2023-01-10 22:10:39 +0700

    added components insinde page/landingPage

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import Banner from '../components/banner/banner';
+import Carousel from '../components/carousel/carousel';
+import Cta from '../components/cta/cta';
+import Footer from '../components/footer/footer';
+import Navbar from '../components/navbar/navbar';
+import Whyus from '../components/whyuse/bannerwhyuse';
+import Popular from '../components/popular/popular';
+import Testimonial from '../components/testimonial/testimonial';
 
 function landingPage() {
-  return <div>landingPage</div>;
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+      <Whyus />
+      <Carousel />
+      <Testimonial />
+      <Cta />
+      <Carousel />
+      <Popular />
+      <Footer />
+    </div>
+  );
 }
 
 export default landingPage;
