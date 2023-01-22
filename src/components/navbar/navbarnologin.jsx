00763fb28d1f4d23c28088e9698359d01127ae08
Finder2111 <76843179+Finder2111@users.noreply.github.com>
2023-01-22 19:52:25 +0700

    Add default navbar (before login)

---
+++ src/components/navbar/navbarnologin.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbarsign = () => {
+    return (
+    <div className='navbar bg-base-100'>
+        <div className='flex-1'>
+            <a href='/landingPage'>
+                <img src='src/assets/LogoNaqosFix.png'></img>
+            </a>
+        </div>
+
+        <div className='flex-none'>
+            <button className='btn btn-outline'>Masuk</button>
+            <button className='btn btn-active'>Daftar</button>
+        </div>
+    </div>
+    )
+}
+
+export default Navbarsign
