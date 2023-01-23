2bd7f12c2b960b166a9808f74f1e4aa8034b1969
Pemuka <undyingwill1@gmail.com>
2023-01-23 16:49:29 +0700

    Removing material tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -23,4 +21,4 @@ module.exports = withMT({
     prefix: "",
     darkTheme: "dark",
   },
-});
+};
