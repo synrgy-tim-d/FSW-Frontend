b2995d197fb33b53faebcd4f67bc7d42ffa03d7a
Reja <rezanurrochmat@gmail.com>
2023-01-25 22:21:37 +0700

    (kucing): add collapse faq

--- src/components/homepage/details-constants/Fasili/Faq.jsx
+++ src/components/homepage/details-constants/Fasili/Faq.jsx
@@ -1,60 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const Faq = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <>
+    <React.Fragment>
       <div className='grid grid-rows-auto gap-6'>
-        <div className='relative border-[#B9B9BC] border-2'>
-          <button
-            className='p-2 pl-4 block text-left w-full text-[#B9B9BC] font-[600] bg-[#FFFFFF] '
-            onClick={() => setIsOpen(!isOpen)}
+        <div className='faq-one'>
+          <div
+            tabIndex={0}
+            className='collapse collapse-plus border border-base-300 bg-base-100 rounded-box'
           >
-            FAQ ?
-          </button>
-          {isOpen.dropdownFaq1 && (
-            <div className='left-0 mt-2 py-2 w-full bg-white'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quasi numquam sed
-              iusto itaque, error distinctio nesciunt temporibus cupiditate laborum beatae rem
-              aperiam labore impedit, in perspiciatis ratione nulla recusandae!
+            <input type='checkbox' />
+            <div className='collapse-title text-sm font-medium pl-2'>FAQ</div>
+            <div className='collapse-content'>
+              <div className='left-0 mt-2 py-2 w-full bg-white'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quasi numquam sed
+                iusto itaque, error distinctio nesciunt temporibus cupiditate laborum beatae rem
+                aperiam labore impedit, in perspiciatis ratione nulla recusandae!
+              </div>
             </div>
-          )}
+          </div>
         </div>
-
-        <div className='relative border-[#B9B9BC] border-2'>
-          <button
-            className='p-2 pl-4 block text-left w-full text-[#B9B9BC] font-[600] bg-[#FFFFFF] '
-            onClick={() => setIsOpen(!isOpen)}
+        <div className='faq-two'>
+          <div
+            tabIndex={0}
+            className='collapse collapse-plus border border-base-300 bg-base-100 rounded-box'
           >
-            FAQ ?
-          </button>
-          {isOpen && (
-            <div className='left-0 mt-2 py-2 w-full bg-white'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quasi numquam sed
-              iusto itaque, error distinctio nesciunt temporibus cupiditate laborum beatae rem
-              aperiam labore impedit, in perspiciatis ratione nulla recusandae!
+            <input type='checkbox' />
+            <div className='collapse-title text-sm font-medium pl-2'>FAQ</div>
+            <div className='collapse-content'>
+              <div className='left-0 mt-2 py-2 w-full bg-white'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quasi numquam sed
+                iusto itaque, error distinctio nesciunt temporibus cupiditate laborum beatae rem
+                aperiam labore impedit, in perspiciatis ratione nulla recusandae!
+              </div>
             </div>
-          )}
-        </div>
-
-        <div className='relative border-[#B9B9BC] border-2'>
-          <button
-            className='p-2 pl-4 block text-left w-full text-[#B9B9BC] font-[600] bg-[#FFFFFF] '
-            onClick={() => setIsOpen(!isOpen)}
-          >
-            FAQ ?
-          </button>
-          {isOpen && (
-            <div className='left-0 mt-2 py-2 w-full bg-white'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quasi numquam sed
-              iusto itaque, error distinctio nesciunt temporibus cupiditate laborum beatae rem
-              aperiam labore impedit, in perspiciatis ratione nulla recusandae!
-            </div>
-          )}
+          </div>
         </div>
       </div>
-    </>
+    </React.Fragment>
   );
 };
 
