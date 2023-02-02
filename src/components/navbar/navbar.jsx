2ecc88a4a5cf84e385eb5e2cfe9086de218c68b0
Finder2111 <finderyehezkiel@yahoo.com>
2023-02-02 15:18:49 +0700

    Notification move position

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -22,11 +22,11 @@ const Navbar = () => {
           <li>
             <label htmlFor='notification-modal' className='font-[600]'>Notifikasi</label>
             <input type='checkbox' id='notification-modal' className='modal-toggle' />
-              <div className="modal">
-                <div className="modal-box bg-white relative">
-                  <label htmlFor="notification-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                  <h3 className="text-lg font-bold">Notifikasi</h3>
-                  <p className="py-4">Belum ada notifikasi untuk kamu</p>
+              <div className='modal'>
+                <div className='modal-box w-[433px] h-[610px] bg-gray-200 absolute top-12 right-16'>
+                  <label htmlFor='notification-modal' className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                  <h2 className='text-lg font-bold absolute left-4 top-2'>Notifikasi</h2>
+                    <div className='absolute text-center h-5 top-[295px] left-12'>Belum ada notifikasi buat kamu</div>
                 </div>
               </div>
           </li>
