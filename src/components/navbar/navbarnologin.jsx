1a02e87b6cca913a038e0c756b7c5dafa476fb3d
Finder2111 <finderyehezkiel@yahoo.com>
2023-02-03 02:24:41 +0700

    Modal login signup no picture

--- src/components/navbar/navbarnologin.jsx
+++ src/components/navbar/navbarnologin.jsx
@@ -1,29 +1,49 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import LogoNaqos from '../../assets/LogoNaqosPolos.png';
 
 const Navbarsign = () => {
     return (
-    <div className='navbar bg-base-100'>
-        <div className='flex-1'>
+    <div className='navbar grid grid-cols-6 bg-[#FAFAFA] font-[Montserrat] font-[600]'>
+        <div className='col-span-1 pl-4'>
             <a href='/landingPage'>
-                <img className='h-14' src='src/assets/LogoNaqosFix.png'></img>
+                <img className='w-full h-auto' src={ LogoNaqos } alt="" />
             </a>
         </div>
 
-        <div className='flex flex-wrap justify-end'>
-            {/* <button className='btn btn-outline'>Masuk</button> */}
-            {/* <button className='btn btn-active'>Daftar</button> */}
-            <Link to='/login' >
-                <button type='button' className='btn btn-outline'>
-                    Masuk
-                </button>
-            </Link>
+        <div className='col-span-5 flex justify-end pr-4 space-x-6'>
+            {/* Button Masuk */}
+            <label htmlFor='login-modal' className='btn btn-outline border-2 border-[#0A008A] rounded-[4px] text-[#0A008A] text-[16px] px-6'>
+                Masuk
+            </label>
+            <input type='checkbox' id='login-modal' className='modal-toggle' />
+            <div className='modal'>
+                {/* <div className='modal-box w-[664px] h-[491px] absolute drop-shadow bg-white top-4'>
+                    <label htmlFor='login-modal' className='btn btn-sm btn-circle absolute right-2 top-2'>✕</label>
+                    <h3 className='text-lg font-bold text-center'>Masuk sebagai</h3>
+                </div> */}
+                <div className='modal-box absolute drop-shadow bg-white p-12 rounded-[12px] grid gap-4'>
+                    <label htmlFor='login-modal' className='btn btn-sm btn-circle absolute right-2 top-2'>✕</label>
+                    <h3 className='text-lg font-bold text-center'>Masuk sebagai</h3>
+                    <Link to='/login'><button className='btn btn-outline'>Pencari Kos</button></Link>
+                    <Link to='/login'><button className='btn btn-outline'>Pemilik Kos</button></Link>
+                </div>
+            </div>
 
-            <Link to='/register' >
-                <button type='button' className='btn btn-active'>
-                    Daftar
-                </button>
-            </Link>
+            {/* Button Daftar */}
+            <label htmlFor='signup-modal' className='btn btn-active bg-[#0A008A] rounded-[4px] text-white text-[16px] px-6'>
+                Daftar
+            </label>
+            <input type='checkbox' id='signup-modal' className='modal-toggle' />
+            <div className='modal'>
+                <div className='modal-box absolute drop-shadow bg-white p-12 rounded-[12px] grid gap-4'>
+                    <label htmlFor='signup-modal' className='btn btn-sm btn-circle absolute right-2 top-2'>✕</label>
+                    <h3 className='text-lg font-bold text-center'>Daftar sebagai</h3>
+                    <Link to='/register'><button className='btn btn-outline'>Pencari Kos</button></Link>
+                    <Link to='/register'><button className='btn btn-outline'>Pemilik Kos</button></Link>
+                </div>
+
+            </div>
         </div>
     </div>
     )
