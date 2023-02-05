ff082a8ef011cfc1a6926c71c9bf404e5e3470bf
Finder2111 <finderyehezkiel@yahoo.com>
2023-02-06 03:51:57 +0700

    Update navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoNaqos from '../../assets/LogoNaqosPolos.png';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className='navbar grid grid-cols-6 bg-[#FAFAFA] font-montserrat font-[400]'>
+    <div className='navbar grid grid-cols-6 bg-[#FAFAFA] font-montserrat font-[400] lg:max-w-7xl lg:mx-auto max-w-full'>
+      {/* Logo Brand */}
       <div className='col-span-1 pl-4'>
         <Link to='/'>
           <img className='w-full h-auto' src={LogoNaqos} />
         </Link>
       </div>
-      <div className='col-span-5 flex justify-end pr-4'>
+
+      {/* Button hamburger menu */}
+      <button className='btn-square rounded-lg md:hidden focus:outline-none focus:shadow-outline' onClick={() => setOpen(!open)}>
+        {open ? (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+            >
+                <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                />
+            </svg>
+        ) : (
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                />
+            </svg>
+        )}
+      </button>
+
+      {/* Navigation */}
+      <div className='lg:block hidden col-span-5 flex justify-end pr-4'>
         <ul className='menu menu-horizontal px-1 grid grid-flow-col gap-6 text-[#000000]/[0.38] text-[20px] '>
           <li>
             <a className='font-[600]'>Wishlist</a>
@@ -24,7 +62,7 @@ const Navbar = () => {
             <label htmlFor='notification-modal' className='font-[600]'>Notifikasi</label>
             <input type='checkbox' id='notification-modal' className='modal-toggle' />
               <div className='modal'>
-                <div className='modal-box w-[433px] h-[610px] bg-white drop-shadow-md absolute top-14 right-16'>
+                <div className='modal-box w-[433px] h-[610px] bg-white drop-shadow-md absolute top-20 right-44'>
                   <label htmlFor='notification-modal' className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                   <h2 className='text-lg font-bold absolute left-4 top-2'>Notifikasi</h2>
                     <div className='absolute text-center h-5 top-[295px] left-12'>Belum ada notifikasi buat kamu</div>
