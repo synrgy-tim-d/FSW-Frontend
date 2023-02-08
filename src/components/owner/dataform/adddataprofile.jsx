923675ba69d883d62079a7dcd67ebadc588ce5fd
Finder2111 <finderyehezkiel@yahoo.com>
2023-02-08 20:39:26 +0700

    Update adddataprofile.jsx

--- src/components/owner/dataform/adddataprofile.jsx
+++ src/components/owner/dataform/adddataprofile.jsx
@@ -7,6 +7,12 @@ import BRI from '../../../assets/LogoBRI.png'
 import BTN from '../../../assets/LogoBTN.png'
 
 const AddOwnerProfile = () => {
+    const [selectedPayment, setSelectedPayment] = useState();
+
+    const handleChange = (e) => {
+        setSelectedPayment(e.target.value);
+    };
+
     return (
         <React.Fragment>
         <div className='grid grid-cols-5 bg-[#FAFAFA]'>
@@ -41,7 +47,7 @@ const AddOwnerProfile = () => {
                 <label htmlFor="" className='relative w-full cursor-pointer pb-10'>
                     <div className='pt-4'>
                         <div className='flex items-center p-5 pr-16 h-[55px] bg border-2 rounded-[100px]'>
-                            <input type='radio' id='BCA' name='payment' />
+                            <input type='radio' name='payment' id='BCA' value='BCA' checked={selectedPayment === 'BCA'} onChange={handleChange} />
                             <img className='h-[55px]' src={BCA} alt='BCA' />
                             <p className='pl-4 font-semibold'>Bank BCA</p>
                         </div>
@@ -51,7 +57,7 @@ const AddOwnerProfile = () => {
                 <label htmlFor="" className='relative w-full cursor-pointer'>
                     <div className='pt-2'>
                         <div className='flex items-center p-5 pr-16 h-[55px] bg border-2 rounded-[100px]'>
-                            <input type='radio' id='Mandiri' name='payment' />
+                            <input type='radio' name='payment' id='Mandiri' value='Mandiri' checked={selectedPayment === 'Mandiri'} onChange={handleChange} />
                             <img className='pl-2 h-[20px]' src={Mandiri} alt='Mandiri' />
                             <p className='pl-4 font-semibold'>Bank Mandiri</p>
                         </div>
@@ -61,7 +67,7 @@ const AddOwnerProfile = () => {
                 <label htmlFor="" className='relative w-full cursor-pointer'>
                     <div className='pt-2'>
                         <div className='flex items-center p-5 pr-16 h-[55px] bg border-2 rounded-[100px]'>
-                            <input type='radio' id='BNI' name='payment' />
+                            <input type='radio' name='payment' id='BNI' value='BNI' checked={selectedPayment === 'BNI'} onChange={handleChange} />
                             <img className='pl-2 h-[20px]' src={BNI} alt='BNI' />
                             <p className='pl-4 font-semibold'>Bank BNI</p>
                         </div>
@@ -71,7 +77,7 @@ const AddOwnerProfile = () => {
                 <label htmlFor="" className='relative w-full cursor-pointer'>
                     <div className='pt-2'>
                         <div className='flex items-center p-5 pr-16 h-[55px] bg border-2 rounded-[100px]'>
-                            <input type='radio' id='BRI' name='payment' />
+                            <input type='radio' name='payment' id='BRI' value='BRI' checked={selectedPayment === 'BRI'} onChange={handleChange} />
                             <img className='pl-2 h-[20px]' src={BRI} alt='BRI' />
                             <p className='pl-4 font-semibold'>Bank BRI</p>
                         </div>
@@ -81,7 +87,7 @@ const AddOwnerProfile = () => {
                 <label htmlFor="" className='relative w-full cursor-pointer'>
                     <div className='pt-2'>
                         <div className='flex items-center p-5 pr-16 h-[55px] bg border-2 rounded-[100px]'>
-                            <input type='radio' id='BTN' name='payment' />
+                            <input type='radio' name='payment' id='BTN' value='BTN' checked={selectedPayment === 'BTN'} onChange={handleChange} />
                             <img className='pl-2 h-[20px]' src={BTN} alt='BTN' />
                             <p className='pl-4 font-semibold'>Bank BTN</p>
                         </div>
@@ -91,14 +97,14 @@ const AddOwnerProfile = () => {
                 <label htmlFor="" className='relative w-full cursor-pointer'>
                     <div className='pt-2 pb-10'>
                         <div className='flex items-center p-5 pr-16 h-[55px] bg border-2 rounded-[100px]'>
-                            <input type='radio' id='other' name='payment' />
+                            <input type='radio' name='payment' id='other' value='other' checked={selectedPayment === 'other'} onChange={handleChange}/>
                             <p className='pl-2 pr-4 font-semibold'>Lainnya</p>
                         </div>
                     </div>
                 </label>
 
                 <label className="relative block pb-10">
-                    <span className="block text-[20px] font-semibold text-[#B9B9BC]">Rekening Bank Lain/Pengelola</span>
+                    <span className={ selectedPayment === 'other' ? 'text-[#000000]' : 'text-[#B9B9BC]'}>Rekening Bank Lain/Pengelola</span>
                     <span className="block text-[20px] text-[#B9B9BC] pb-3">Rekening bank lain yang tidak tertera diatas</span>
 
                     <input className="placeholder:text-base placeholder:font-semibold placeholder:text-[#46464F]/[0.38] block bg-white w-full border border-slate-300 rounded-full py-4 pl-6 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Ketikkan nama bank disini" type="text" name="search" />
