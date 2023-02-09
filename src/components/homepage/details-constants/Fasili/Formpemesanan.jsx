cb2f733a4eb31a33ab7593520a47c2cd775e2d4e
Farrel Abyansyah <farrel.abyansyah@gmail.com>
2023-02-09 18:34:54 +0700

    add booking functionality

--- src/components/homepage/details-constants/Fasili/Formpemesanan.jsx
+++ src/components/homepage/details-constants/Fasili/Formpemesanan.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Formpemesanan = () => {
+const Formpemesanan = ({ dateOnChange, totalPayment, timeOnChange, submitOnClick, dateDesc }) => {
   return (
     <React.Fragment>
       <form
@@ -9,35 +9,44 @@ const Formpemesanan = () => {
       >
         <div className='mt-8'>
           <p className='text-[20px] font-[700]'>
-            Rp1.000.000
-            <span className='font-[400] text-[14px]'>/bulan</span>
+            {`Rp.${new Intl.NumberFormat('en-DE').format(totalPayment)}`}
+            <span className='font-[400] text-[14px]'>{`/${dateDesc}`}</span>
           </p>
         </div>
         <div className='w-4/5'>
           <input
-            type='text'
-            className='w-full border-2 border-[#0A008A] text-[#B9B9BC] text-[14px] font-[500] p-1 rounded-[8px]'
+            type='date'
+            className='w-full border-2 border-[#0A008A] text-[#46464f] text-[14px] font-[500] p-1 rounded-[8px]'
             placeholder='Check-in'
+            onChange={dateOnChange}
           />
         </div>
         <div className='w-4/5'>
-          <input
-            type='text'
-            className='w-full border-2 border-[#0A008A] text-[#000000] text-[14px] font-[500] p-1 rounded-[8px]'
-            placeholder='Per Bulan'
-          />
+          <select
+            name='rent_time'
+            className='w-full border-2 border-[#0A008A] text-[#46464f] text-[14px] font-[500] p-1 rounded-[8px]'
+            id=''
+            onChange={timeOnChange}
+          >
+            <option value='HARIAN'>Harian</option>
+            <option value='MINGGUAN'>Mingguan</option>
+            <option value='BULANAN'>Bulanan</option>
+          </select>
         </div>
         <div className='w-full px-6'>
           <hr className='w-full border-2 border-dashed boreder-[#46464F]/[0.38]' />
         </div>
         <div className='w-full grid grid-cols-2 grid-flow-col px-6'>
           <div className='flex justify-start'>Total</div>
-          <div className='flex justify-end'>Rp1.000.000</div>
+          <div className='flex justify-end'>{`Rp.${new Intl.NumberFormat('en-DE').format(
+            totalPayment,
+          )}`}</div>
         </div>
         <div className='w-full px-6'>
           <button
             type='submit'
-            className='w-full bg-[#46464F1F]/[0.12] text-[#46464F1F]/[0.38] text-[16px] font-[600] mb-8 p-2'
+            className='bg-[#0A008A] mb-8 p-2 text-[16px] text-white rounded w-full py-1 lg:py-2 px-12 hover:bg-[#A0A3FF] hover:text-[#FFFFFF] active:bg-black'
+            onClick={submitOnClick}
           >
             Sewa
           </button>
