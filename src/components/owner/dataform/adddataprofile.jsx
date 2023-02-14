6f36144b9080eacda3e2a588ce416416c043f435
Finder2111 <finderyehezkiel@yahoo.com>
2023-02-14 18:37:55 +0700

    Update adddataprofile.jsx

--- src/components/owner/dataform/adddataprofile.jsx
+++ src/components/owner/dataform/adddataprofile.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import BCA from '../../../assets/LogoBCA.png'
 import Mandiri from '../../../assets/LogoMandiri.png'
 import BNI from '../../../assets/LogoBNI.png'
 import BRI from '../../../assets/LogoBRI.png'
 import BTN from '../../../assets/LogoBTN.png'
+import { useState } from 'react';
 
 const bankList = ["BCA","Mandiri", "BNI", "BRI", "BTN","Lainnya"]
 const bankImages = {
@@ -28,49 +29,31 @@ const AddOwnerProfile = () => {
                 <SidebarForm />
             </div>
 
-        <div className='col-span-4 grid grid-rows-auto w-2/3 justify-self-left pl-14 gap-y-12 py-12'>
-          <div className=''>
-            <p className='text-[39px] text-[#000000] font-[600]'>
-              Silahkan Lengkapi Data Pemilik/Pengelola Kos
-            </p>
-            <p className='text-[20px] text-[#000000]'>
-              Data pemilik digunakan apabila calon penyewa akan melakukan negosiasi dan pembayaran
-            </p>
-          </div>
+            <div className='col-span-4 grid grid-rows-auto w-2/3 justify-self-left pl-14 gap-y-12 py-12'>
 
-          <form>
-            <label className='relative block pb-10'>
-              <span className='block text-[20px] font-semibold text-[#000000] pb-3'>
-                Nama Pemilik/Pengelola
-              </span>
+            <div className=''>
+                <p className='text-[39px] text-[#000000] font-[600]'>Silahkan Lengkapi Data Pemilik/Pengelola Kos</p>
+                <p className='text-[20px] text-[#000000]'>Data pemilik digunakan apabila calon penyewa akan melakukan negosiasi dan pembayaran</p>
+            </div>
 
-              <input
-                className='placeholder:text-base placeholder:font-semibold placeholder:text-[#46464F]/[0.38] block bg-white w-full border border-slate-300 rounded-full py-4 pl-6 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
-                placeholder='Ketikkan nama lengkapmu disini'
-                type='text'
-                name='search'
-              />
-            </label>
 
-            <label className='relative block pb-10'>
-              <span className='block text-[20px] font-semibold text-[#000000] pb-3'>
-                Nomor HP (WhatsApp) Pemilik/Pengelola
-              </span>
+            <form>
+                <label className="relative block pb-10">
+                    <span className="block text-[20px] font-semibold text-[#000000] pb-3">Nama Pemilik/Pengelola</span>
 
-              <input
-                className='placeholder:text-base placeholder:font-semibold placeholder:text-[#46464F]/[0.38] block bg-white w-full border border-slate-300 rounded-full py-4 pl-6 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
-                placeholder='Isi nomor handphone Whatsapp aktif'
-                type='text'
-                name='search'
-              />
-            </label>
+                    <input className="placeholder:text-base placeholder:font-semibold placeholder:text-[#46464F]/[0.38] block bg-white w-full border border-slate-300 rounded-full py-4 pl-6 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Ketikkan nama lengkapmu disini" type="text" name="search" />
+                </label>
 
-            <label className='relative block'>
-              <span className='block text-[20px] font-semibold text-[#000000]'>Rekening Bank</span>
-              <span className='block text-[20px] text-[#000000]'>
-                Rekening yang digunakan untuk pembayaran melalui transfer
-              </span>
-            </label>
+                <label className="relative block pb-10">
+                    <span className="block text-[20px] font-semibold text-[#000000] pb-3">Nomor HP (WhatsApp) Pemilik/Pengelola</span>
+
+                    <input className="placeholder:text-base placeholder:font-semibold placeholder:text-[#46464F]/[0.38] block bg-white w-full border border-slate-300 rounded-full py-4 pl-6 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Isi nomor handphone Whatsapp aktif" type="text" name="search" />
+                </label>
+
+                <label className="relative block">
+                    <span className="block text-[20px] font-semibold text-[#000000]">Rekening Bank</span>
+                    <span className="block text-[20px] text-[#000000]">Rekening yang digunakan untuk pembayaran melalui transfer</span>
+                </label>
 
                 {bankList.map((e,i) => {
                     return (
@@ -93,54 +76,38 @@ const AddOwnerProfile = () => {
                     <input className="placeholder:text-base placeholder:font-semibold placeholder:text-[#46464F]/[0.38] block bg-white w-full border border-slate-300 rounded-full py-4 pl-6 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Ketikkan nama bank disini" type="text" name="search" disabled={selectedBank!="Lainnya"}/>
                 </label>
 
-            <label className='relative block pb-10'>
-              <span className='block text-[20px] font-semibold text-[#000000] pb-3'>
-                Nomor Rekening
-              </span>
+                <label className="relative block pb-10">
+                    <span className="block text-[20px] font-semibold text-[#000000] pb-3">Nomor Rekening</span>
 
-              <input
-                className='placeholder:text-base placeholder:font-semibold placeholder:text-[#46464F]/[0.38] block bg-white w-full border border-slate-300 rounded-full py-4 pl-6 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
-                placeholder='Isi nomor rekening bank'
-                type='text'
-                name='search'
-              />
-            </label>
+                    <input className="placeholder:text-base placeholder:font-semibold placeholder:text-[#46464F]/[0.38] block bg-white w-full border border-slate-300 rounded-full py-4 pl-6 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Isi nomor rekening bank" type="text" name="search" />
+                </label>
 
-            <label className='relative block pb-10'>
-              <span className='block text-[20px] font-semibold text-[#000000] pb-3'>
-                Kepemilikan Rekening
-              </span>
+                <label className="relative block pb-10">
+                    <span className="block text-[20px] font-semibold text-[#000000] pb-3">Kepemilikan Rekening</span>
 
-              <input
-                className='placeholder:text-base placeholder:font-semibold placeholder:text-[#46464F]/[0.38] block bg-white w-full border border-slate-300 rounded-full py-4 pl-6 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
-                placeholder='Isi nama kepemilikan rekening bank'
-                type='text'
-                name='search'
-              />
-            </label>
-          </form>
-          <div className='grid grid-flow-col place-content-between pt-24'>
+                    <input className="placeholder:text-base placeholder:font-semibold placeholder:text-[#46464F]/[0.38] block bg-white w-full border border-slate-300 rounded-full py-4 pl-6 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Isi nama kepemilikan rekening bank" type="text" name="search" />
+                </label>
+                </form>
+            <div className='grid grid-flow-col place-content-between pt-24'>
             <button
-              className='rounded border-2 border-[#0A008A] text=[#0A008A] bg-white font-[600] p-2 px-3'
-              type='button'
+                className='rounded border-2 border-[#0A008A] text=[#0A008A] bg-white font-[600] p-2 px-3'
+                type='button'
             >
-              Kembali
+                Kembali
             </button>
 
-            <Link to='/owner/kostdata'>
-              <button
+            <button
                 className='rounded border-2 border-[#0A008A] bg-[#0A008A] text-white font-[600] p-2 px-3'
                 type='button'
                 onClick={(e) => {onFormSubmit(e)}}
             >
                 Simpan & Lanjutkan
-              </button>
-            </Link>
-          </div>
+            </button>
+            </div>
         </div>
-      </div>
-    </React.Fragment>
-  );
+        </div>
+        </React.Fragment>
+    );
 };
 
 export default AddOwnerProfile;
